c5b8b85aa86f90eae6104a4385c6113b41725167
djuanit0x <dennisjuanito@gmail.com>
2020-05-10 14:14:32 -0700

    Remove nprogress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,15 +26,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-nprogress`,
-      options: {
-        // Setting a color is optional.
-        color: `tomato`,
-        // Disable the loading spinner.
-        showSpinner: true,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `${process.env.GA_TRACKING_ID}`,
